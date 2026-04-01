5399f201c32a9d28d247536a0b7b05061549a4ac
Alok Kumar <mauryaak13@gmail.com>
2026-04-02 02:49:12 +0530

    Building Nodejs Authentication

--- NodeJs/Short_URL/index.js
+++ NodeJs/Short_URL/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const {connectToMongoDB} = require('./connect')
-const urlRoute = require('./routes/url');
+const {restrictToLoggedinUserOnly, checkAuth} = require("./middlewares/auth");
 const URL = require('./models/url');
-const staticRoute = require("./routes/staticRouter")
 
+const urlRoute = require('./routes/url');
+const staticRoute = require("./routes/staticRouter")
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -17,6 +20,7 @@ app.set('views', path.resolve("./views"));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 // app.get("/test", async (req, res) => {
 //     const allUrls = await URL.find({});
@@ -25,9 +29,10 @@ app.use(express.urlencoded({extended: false}));
 //     });
 // })
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
 
-app.use("/", staticRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
