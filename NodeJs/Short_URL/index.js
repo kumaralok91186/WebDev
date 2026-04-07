57da2e08e9da48b11695f599ac197d94f21569d7
Alok Kumar <mauryaak13@gmail.com>
2026-04-08 02:06:53 +0530

    Desing css of signup, login and home  pages

--- NodeJs/Short_URL/index.js
+++ NodeJs/Short_URL/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const {connectToMongoDB} = require('./connect')
-const {restrictToLoggedinUserOnly, checkAuth} = require("./middlewares/auth");
+const {checkForAuthentication, restrictTo} = require("./middlewares/auth");
 const URL = require('./models/url');
 
 const urlRoute = require('./routes/url');
@@ -21,18 +21,13 @@ app.set('views', path.resolve("./views"));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 
-// app.get("/test", async (req, res) => {
-//     const allUrls = await URL.find({});
-//     return res.render('home', {
-//         urls: allUrls,
-//     });
-// })
+app.use(express.static('public'));
 
-app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRoute);
 app.use("/user", userRoute);
-
-app.use("/", checkAuth, staticRoute);
+app.use("/", staticRoute);
 
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
