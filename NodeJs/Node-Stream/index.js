876b5420545d5b0accc86a737bd53c5345fbb886
Alok Kumar <mauryaak13@gmail.com>
2026-04-19 00:28:31 +0530

    Node.js streams for chunks the data for efficiently handle

---
+++ NodeJs/Node-Stream/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const zlib = require("zlib");
+const status = require("express-status-monitor");
+
+const app = express();
+const PORT = 8000;
+
+app.use(status());
+
+// Stream Read (sample.txt) ---> zipper --> fs Write Stream
+
+fs.createReadStream("./sample.txt").pipe(
+  zlib.createGzip().pipe(fs.createWriteStream("./sample.zip")),
+);
+
+
+app.get("/", (req, res) => {
+
+  // fs.readFile("./sample.txt", (err, data) => {
+  //     res.end(data);
+  // });
+
+  const stream = fs.createReadStream("./sample.txt", "utf-8");
+  stream.on("data", (chunk) => res.write(chunk));
+  stream.on("end", () => res.end());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started at http://localhost:${PORT}`);
+});
